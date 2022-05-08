9ef3e6247fc5b0ec7a3ac258ff38eb9393ce5eaf
VENKATESWARI NANDYALA <venkateswarinandyala@gmail.com>
2022-05-08 20:19:20 +0530

    Tag

--- Projectfrontend/src/app/services/food.service.ts
+++ Projectfrontend/src/app/services/food.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { sample_foods } from 'src/data';
+import { sample_foods, sample_tags } from 'src/data';
 import { Food } from '../shared/models/food';
+import { Tag } from '../shared/Tag';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,15 @@ export class FoodService {
   getAllFoodsBySearchTerm(searchTerm:string){
     return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
   }
+
+  getAllTags(): Tag[] {
+    return sample_tags
+  }
+
+  getAllFoodsByTag(tag: string): Food[] {
+    return tag === "All" ?
+      this.getAll() :
+      this.getAll().filter(food => food.tags?.includes(tag));
+  }
+
 }
