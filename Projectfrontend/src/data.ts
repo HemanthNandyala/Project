ddfc29eb61f80fc439f4113e7cbe93bc9021ee82
VENKATESWARI NANDYALA <venkateswarinandyala@gmail.com>
2022-05-08 19:44:50 +0530

    project

---
+++ Projectfrontend/src/data.ts
@@ -0,0 +1,46 @@
+import {Food} from './app/shared/models/food' ;
+
+export const sample_foods: Food[] =[
+  {
+    id: 1,
+    name: 'Pizza Pepperoni',
+    price: 10,
+    imageUrl: '/assets/images/food-1.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch'],
+  },
+  {
+    id: 2,
+    name: 'Meatball',
+    price: 20,
+    imageUrl: '/assets/images/food-2.jpg',
+    tags: ['SlowFood', 'Lunch'],
+  },
+  {
+    id: 3,
+    name: 'Hamburger',
+    price: 5,
+    imageUrl: '/assets/images/food-3.jpg',
+    tags: ['FastFood', 'Hamburger'],
+  },
+  {
+    id: 4,
+    name: 'Fried Potatoes',
+    price: 2,
+    imageUrl: '/assets/images/food-4.jpg',
+    tags: ['FastFood', 'Fry'],
+  },
+  {
+    id: 5,
+    name: 'Chicken Soup',
+    price: 11,
+    imageUrl: '/assets/images/food-5.jpg',
+    tags: ['SlowFood', 'Soup'],
+  },
+  {
+    id: 6,
+    name: 'Vegetables Pizza',
+    price: 9,
+    imageUrl: '/assets/images/food-6.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch'],
+  },
+]
